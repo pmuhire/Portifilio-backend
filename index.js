9f0d8af6c77f0350f69a3ac25fb4c837d8166dac
pmuhire2002 <pmuhire2002@gmail.com>
2022-04-26 15:16:34 +0200

    Configuring deployment

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ mongoose.connect(process.env.MONGODB_URI, {
 }).then(()=>{
     const app=express();
 
-    app.use(cors);
+    app.use(cors());
     app.use(express.json());
     app.use('/api-docs',swaggerUiExpress.serve,swaggerUiExpress.setup(docs));
     app.use("/api",router);
